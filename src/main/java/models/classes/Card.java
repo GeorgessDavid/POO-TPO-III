0883e4e868ab0cd7ed1bc101695a40fe84014224
santiGalarza <s4n7ini7o@gmail.com>
2025-06-05 16:41:24 -0300

    refactor Card

--- src/main/java/models/classes/Card.java
+++ src/main/java/models/classes/Card.java
@@ -1,6 +1,9 @@
 package models.classes;
+
 import models.enums.CardType;
 import models.classes.Expense;
+
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -10,7 +13,7 @@ public abstract class Card{
     String cardNumber;
     String cvc;
     String expiryDate;
-    CardType type;
+    List<Expense> consumos;
 
     public Card(String name, String lastname, String cardNumber, String cvc, String expiryDate) {
         this.name = name;
@@ -18,7 +21,7 @@ public abstract class Card{
         this.cardNumber = cardNumber;
         this.cvc = cvc;
         this.expiryDate = expiryDate;
-        this.type = type;
+        consumos = new ArrayList<>();
     }
 
     protected abstract double calcularConsumo(List<Expense> consumoTotal);
@@ -26,15 +29,35 @@ public abstract class Card{
     protected double sumarConsumos(List<Expense> consumos){
         if(consumos.isEmpty()) return 0.00;
 
-        if(!consumos.isEmpty()){
-            double consumoTotal= 0.00;
-            for(int i = 0; i > consumos.size(); i++){
-                double consumo = consumos.get(i).getExpense();
-                consumoTotal += consumo;
-            }
-            return consumoTotal;
+        double consumoTotal= 0.00;
+        for(int i = 0; i < consumos.size(); i++){
+            double consumo = consumos.get(i).getExpense();
+            consumoTotal += consumo;
         }
+        return consumoTotal;
+    }
 
-        return 0.00;
+    public String getName() {
+        return name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public String getCvc() {
+        return cvc;
+    }
+
+    public String getExpiryDate() {
+        return expiryDate;
+    }
+
+    public List<Expense> getConsumos() {
+        return consumos;
     }
 }
