934d00e4c60d8e8fd8c3961fdc9b8c7b3191e90e
santiGalarza <s4n7ini7o@gmail.com>
2025-06-06 20:16:38 -0300

    refactor: separar logica en tarjetaController & consumoController

---
+++ src/main/java/org/uade/controllers/ConsumoController.java
@@ -0,0 +1,42 @@
+package org.uade.controllers;
+
+import org.uade.dtos.ExpenseDTO;
+import org.uade.exceptions.CardNotFoundException;
+import org.uade.models.CardModel;
+import org.uade.models.ExpenseModel;
+
+public class ConsumoController {
+
+    private static ConsumoController INSTANCE;
+    private final TarjetaController tarjetaController;
+
+    private ConsumoController() {
+        this.tarjetaController = TarjetaController.getInstance();
+    }
+
+    public static ConsumoController getInstance() {
+        if (INSTANCE == null) INSTANCE = new ConsumoController();
+
+        return INSTANCE;
+    }
+
+    public double calcularConsumoRealMes(String numeroTarjeta, String month, String year){
+        CardModel card = tarjetaController.buscarTarjeta(numeroTarjeta);
+
+        if(card == null) throw new CardNotFoundException("La tarjeta no existe en el sistema");
+
+        return card.calcularConsumoRealMes(card.getConsumos(),month, year);
+    }
+
+    public void agregarConsumo(String cardNumber, ExpenseDTO expense) {
+        CardModel tarjeta = tarjetaController.buscarTarjeta(cardNumber);
+
+        if (tarjeta == null) {
+            throw new CardNotFoundException(cardNumber);
+        }
+
+        ExpenseModel consumo = new ExpenseModel(expense.getCard(), expense.getExpense(), expense.getMonth(), expense.getYear(), expense.getPlace());
+        tarjeta.setConsumo(consumo);
+    }
+
+}
--- src/main/java/org/uade/controllers/TarjetaController.java
+++ src/main/java/org/uade/controllers/TarjetaController.java
@@ -4,11 +4,10 @@ import org.uade.dtos.CardDTO;
 import org.uade.dtos.CreditCardDTO;
 import org.uade.dtos.DebitCardDTO;
 import org.uade.exceptions.CardAlreadyRegisteredException;
-import org.uade.exceptions.CardNotFoundException;
 import org.uade.models.CardModel;
 import org.uade.models.CreditCardModel;
 import org.uade.models.DebitCardModel;
-import org.uade.models.ExpenseModel;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,24 +25,6 @@ public class TarjetaController {
         if (instance == null) instance = new TarjetaController();
 
         return instance;
-    }
-
-    public double calcularConsumoRealMes(String numeroTarjeta, String month, String year){
-        CardModel card = buscarTarjeta(numeroTarjeta);
-
-        if(card == null) throw new CardNotFoundException("La tarjeta no existe en el sistema");
-
-        return card.calcularConsumoRealMes(card.getConsumos(),month, year);
-    }
-
-    public void agregarConsumo(String cardNumber, String month, String year, double expense, String businessName) {
-        CardModel tarjeta = buscarTarjeta(cardNumber);
-
-        ExpenseModel consumo = new ExpenseModel(tarjeta, expense, month, year, businessName);
-        if (tarjeta == null) {
-            throw new CardNotFoundException(cardNumber);
-        }
-        tarjeta.setConsumo(consumo);
     }
 
     public CardModel buscarTarjeta(String numeroTarjeta) {
@@ -66,12 +47,11 @@ public class TarjetaController {
         }
 
         if (card instanceof DebitCardDTO) {
-            DebitCardModel debitCard = new DebitCardModel(card.getName(), card.getLastname(), card.getCardNumber(), card.getCvc(), card.getCvc(), ((DebitCardDTO) card).getIva(), card.getClient());
+            DebitCardModel debitCard = new DebitCardModel(card.getName(), card.getLastname(), card.getCardNumber(), card.getCvc(), card.getExpiryDate(), ((DebitCardDTO) card).getIva(), card.getClient());
             this.tarjetas.add(debitCard);
         }
 
     }
-
 
     private boolean verifyByCardNumber(String cardNumber){
         for (CardModel tarjeta : tarjetas) {
